c0cbea47d814064f406f14eab35a9387aee3732d
keerthana252003 <keerthiishyam25@gmail.com>
2025-11-10 11:32:52 +0530

    Add files via upload

---
+++ boolean.php
@@ -0,0 +1,6 @@
+<?php
+$is_student =true;
+$is_teacher = false;
+?>
+<h5> Boolean datatype example : boolean value is: <?php echo $is_student; ?></h5>
+<h3> boolean example : boolean value is: <?php echo $is_teacher; ?></h3>
